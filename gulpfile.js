72ddc9bcb6187884648e6bb5d2a2e3917a8a1970
Kevin Bayter <kevinbayter@gmail.com>
2021-06-16 16:46:08 -0500

    se agrega funcion minificar imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,37 @@
-    const { series, src, dest, watch } = require('gulp');
+    const {
+        series,
+        src,
+        dest,
+        watch, parrallel
+    } = require('gulp');
     const sass = require('gulp-sass');
+    const imagemin = require('gulp-imagemin');
+    const notify = require('gulp-notify');
+
 
     function css() {
         return src('src/scss/app.scss')
-        .pipe( sass({outputStyle:'compact'}))
-        .pipe( dest('./build/css'))
+            .pipe(sass({
+                outputStyle: 'compact'
+            }))
+            .pipe(dest('./build/css'))
+    }
+
+    function imagenes() {
+        return src('src/img/**/*')
+            .pipe(imagemin())
+            .pipe(dest('./build/img'))
+            .pipe(notify({
+                message: 'Imagen minificada'
+            }));
     }
 
     function watchArchivos() {
-    watch('src/scss/**/*.scss', css); /*  doble ** quiere decir todos los archivos con esa extension */
+        watch('src/scss/**/*.scss', css); /*  doble ** quiere decir todos los archivos con esa extension */
     }
 
-exports.css = css;
+    exports.css = css;
+    exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
+    
+exports.defaults = series(css, imagenes, watchArchivos);
