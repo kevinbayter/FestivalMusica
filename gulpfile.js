e864f9ce9893c5b57d0458fe4d6242fc188ce554
Kevin Bayter <kevinbayter@gmail.com>
2021-07-08 15:30:02 -0500

    Sin commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,56 @@
-    const {
-        series,
-        src,
-        dest,
-        watch, parrallel
-    } = require('gulp');
-    const sass = require('gulp-sass');
-    const imagemin = require('gulp-imagemin');
-    const notify = require('gulp-notify');
+const { series, src, dest, watch, parrallel } = require("gulp");
+const sass = require("gulp-sass")(require("node-sass"));
+const imagemin = require("gulp-imagemin");
+const notify = require("gulp-notify");
+const webp = require("gulp-webp");
 
+/* Funciones que compila sass */
+const paths = {
+  imagenes: "src/img/**/*",
+  scss: "src/scss/**/*.scss",
+};
 
-    function css() {
-        return src('src/scss/app.scss')
-            .pipe(sass({
-                outputStyle: 'compact'
-            }))
-            .pipe(dest('./build/css'))
-    }
+function css() {
+  return src(paths.scss)
+    .pipe(
+      sass({
+        outputStyle: "compact",
+      })
+    )
+    .pipe(dest("./build/css"));
+}
 
-    function imagenes() {
-        return src('src/img/**/*')
-            .pipe(imagemin())
-            .pipe(dest('./build/img'))
-            .pipe(notify({
-                message: 'Imagen minificada'
-            }));
-    }
+function imagenes() {
+  return src(paths.imagenes)
+    .pipe(imagemin())
+    .pipe(dest("./build/img"))
+    .pipe(
+      notify({
+        message: "Imagen Minificada",
+      })
+    );
+}
 
-    function watchArchivos() {
-        watch('src/scss/**/*.scss', css); /*  doble ** quiere decir todos los archivos con esa extension */
-    }
+function versionWebp() {
+  return src(paths.imagenes)
+    .pipe(webp())
+    .pipe(dest("./build/img"))
+    .pipe(
+      notify({
+        message: "Version webp lista",
+      })
+    );
+}
 
-    exports.css = css;
-    exports.imagenes = imagenes;
+function watchArchivos() {
+  watch(
+    paths.scss,
+    css
+  ); /*  doble ** quiere decir todos los archivos con esa extension */
+}
+
+exports.css = css;
+exports.imagenes = imagenes;
 exports.watchArchivos = watchArchivos;
-    
-exports.defaults = series(css, imagenes, watchArchivos);
+
+exports.default = series(css, imagenes, versionWebp, watchArchivos);
